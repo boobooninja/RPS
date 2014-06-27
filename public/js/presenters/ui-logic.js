e2972bcb588bac9c84e03c3f04f8ea93bcb98f29
Paul Dombkowski <pdombkowski@gmail.com>
2014-06-27 16:10:44 -0500

    WIP javascrip business and ui logic

--- public/js/presenters/ui-logic.js
+++ public/js/presenters/ui-logic.js
@@ -2,26 +2,6 @@
 
   var PresentationUI = function() {
 
-    // var joke = {};
-
-    // $(document).on("click", '#new-joke', function() {
-    //   joke = bl.getRandJokeObject();
-
-    //   if (joke) {
-    //     $('.jokes-wrapper h3').empty().text(joke['joke']);
-    //     $('#show-answer').show();
-    //     $('.jokes-answer-wrapper').empty();
-    //   } else {
-    //     $('.jokes-wrapper h3').empty().text("No jokes are available at this time.  Try again in a few seconds...");
-    //   }
-
-    // });
-
-    // $(document).on("click", "#show-answer", function() {
-    //   // make sure to escape any code in your jokes and answers by using acsii characters in the yaml or db
-    //   // since we are telling programming jokes it is a distinct possibility
-    //   $('.jokes-answer-wrapper').html("<strong>Answer: </strong>" + joke['answer']);
-    // });
 
     // $(document).on("click", "#add-new-joke-submit", function(){
     //   var joke = $('#new-joke-input').val()
