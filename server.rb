6c150f86f2eaad1a22931bf7625c28ff0004bba5
Paul Dombkowski <pdombkowski@gmail.com>
2014-06-27 12:49:14 -0500

    merged server.rb file from Loren and mine

--- server.rb
+++ server.rb
@@ -11,10 +11,9 @@ set :bind, '0.0.0.0' # Vagrant fix
 set :port, 9494
 
 get '/' do
-  result = RPS::ValidateSession.run(:session_id => session[:session_id])
-  @errors = result[:errors]
-
+  result = RPS::ValidateSession.run(params)
   if result[:success?]
+    @errors = result[:errors]
     @player = result[:player]
     erb :home
   else
@@ -23,7 +22,7 @@ get '/' do
 end
 
 get '/signup' do
-  result = RPS::ValidateSession.run(:session_id => session[:session_id])
+  result = RPS::ValidateSession.run(params)
   @errors = result[:errors]
 
   if result[:success?]
@@ -35,7 +34,7 @@ get '/signup' do
 end
 
 post '/signup' do
-  result = RPS::ValidateSession.run(:session_id => session[:session_id])
+  result = RPS::ValidateSession.run(params)
   @errors = result[:errors]
 
   if result[:success?]
@@ -43,15 +42,15 @@ post '/signup' do
     @player = result[:player]
     erb :home
   else
-    result = RPS::SignUp.run(:name => params[:name], :username => params[:username], :password => params[:password])
+    result = RPS::SignUp.run(params)
     @errors.push(result[:errors]).flatten
 
     if result[:success?]
-      result = RPS::SignIn.run(:username => params[:username], :password => params[:password])
+      result = RPS::SignIn.run(params)
       @errors.push(result[:errors]).flatten
 
       if result[:success?]
-        session[:session_id] = result[:session_id]
+        session[:rps_session_id] = result[:rps_session_id]
         @player = result[:player]
         erb :home
       else
@@ -64,14 +63,14 @@ post '/signup' do
 end
 
 get '/login' do
-  result = RPS::ValidateSession.run(:session_id => session[:session_id])
+  result = RPS::ValidateSession.run(params)
   @errors = result[:errors]
 
   if result[:success?]
     @player = result[:player]
     erb :home
   else
-    result = RPS::SignIn.run(:username => params[:username], :password => params[:password])
+    result = RPS::SignIn.run(params)
     @errors.push(result[:errors]).flatten
 
     if result[:success?]
@@ -85,7 +84,7 @@ get '/login' do
 end
 
 get '/signout' do
-  result = RPS::DeleteSession.run(:session_id => session[:session_id])
+  result = RPS::DeleteSession.run(params)
   @errors = result[:errors]
 
   if result[:success?]
@@ -97,13 +96,14 @@ get '/signout' do
 end
 
 get '/match/:match_id/game/:game_id' do
-  result = RPS::ValidateSession.run(:session_id => session[:session_id])
+  result = RPS::ValidateSession.run(params)
   @errors = result[:errors]
 
   if result[:success?]
-    @player = result[:player]
+    @player   = result[:player]
+    # @opponent = nil
     @match  = RPS.db.find('matches, playermatches',{:player_id => @player.id, :completed_at => null}).first
-    @games  = RPS.db.find('games, moves',{:match_id => @match.id})
+    @game   = RPS.db.find('games, moves',{:match_id => @match.id}).first
     erb :game
   else
     erb :login
